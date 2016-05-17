221c47e8ba17d7530487107d2637041819b2407d
Maximilian Alexander <max@zinkpulse.com>
2016-05-16 19:26:34 -0700

    annotation view

--- lib/src/main/java/supermaps/supermaps/lib/Annotation.java
+++ lib/src/main/java/supermaps/supermaps/lib/Annotation.java
@@ -6,6 +6,7 @@ import java.util.Observable;
 
 /**
  * Created by maximilianalexander on 5/7/16.
+ * This is equivalent to an index in RecyclerView. Since MapViews don't have index, we need a object that hosts a LatLng
  */
 public interface Annotation {
 
--- lib/src/main/java/supermaps/supermaps/lib/AnnotationView.java
+++ lib/src/main/java/supermaps/supermaps/lib/AnnotationView.java
@@ -2,13 +2,12 @@ package supermaps.supermaps.lib;
 
 /**
  * Created by maximilianalexander on 5/7/16.
+ * This should be an abstract class that hosts the view presentable on the map.
+ * It will have a reference to the annotation that it represent.
  */
-public interface AnnotationView {
+public abstract class AnnotationView {
 
-    String getReuseId();
-    void setReuseId();
-
-    Annotation getAnnotation();
-    void setAnnotation();
+    abstract Annotation getAnnotation();
+    abstract void setAnnotation(Annotation annotation);
 
 }
