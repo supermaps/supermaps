cfae5cd6a569f493ddd32dc4dd94a227db86602c
Maximilian Alexander <max@zinkpulse.com>
2016-05-24 21:03:12 -0700

    adding comments to readme

--- app/src/main/java/supermaps/supermap/MainActivity.java
+++ app/src/main/java/supermaps/supermap/MainActivity.java
@@ -3,11 +3,16 @@ package supermaps.supermap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import supermaps.supermaps.lib.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMapView = (MapView)findViewById(R.id.map_view);
     }
 }
