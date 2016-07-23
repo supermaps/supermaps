cc1fb7880aa403f15d6f87cc9acacf3d221e9999
arioch666 <divij.durve@gmail.com>
2016-07-22 22:50:50 -0700

    Demo app - update
    The demo app is using the current version of the library associated with it. However, the view does not seem to load a map. the onMapReady call back is not working. We are abandoning this branch and attepting to work with the mapFragment instead.

--- app/src/main/java/supermaps/supermap/MainActivity.java
+++ app/src/main/java/supermaps/supermap/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSuperMap = (SuperMap)findViewById(R.id.map_view);
+        this.mSuperMap.commonInit();
     }
 }
