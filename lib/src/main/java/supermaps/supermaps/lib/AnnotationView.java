73741195307793a838ba7bddf477915e7a2c364d
arioch666 <divij.durve@gmail.com>
2016-08-25 00:43:04 -0700

    Changed AnnotationView abstract class so it is a viewgroup
    Changed to viewgroup so we can add views to it.

--- lib/src/main/java/supermaps/supermaps/lib/AnnotationView.java
+++ lib/src/main/java/supermaps/supermaps/lib/AnnotationView.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by maximilianalexander on 5/7/16.
  * This should be an abstract class that hosts the view presentable on the map.
  * It will have a reference to the annotation that it represent.
  */
-public abstract class AnnotationView extends View {
+public abstract class AnnotationView extends ViewGroup {
 
     public abstract Annotation getAnnotation();
     public abstract void setAnnotation(Annotation annotation);
