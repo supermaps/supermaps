5ddaba1e0d1c0ae3718ebf2c40da2853d90af5eb
arioch666 <divij.durve@gmail.com>
2016-09-09 19:29:53 -0700

    BoatAnnotationView - started implementation

--- app/src/main/java/supermaps/supermap/AnnotationViews/BoatAnnotationView.java
+++ app/src/main/java/supermaps/supermap/AnnotationViews/BoatAnnotationView.java
@@ -1,13 +1,20 @@
 package supermaps.supermap.AnnotationViews;
 
 import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import supermaps.supermap.R;
 import supermaps.supermaps.lib.Annotation;
 import supermaps.supermaps.lib.AnnotationView;
+import supermaps.supermaps.lib.SuperMap;
 
 /**
  * Created by ariochdivij666 on 8/24/16.
+ * TODO FINISH THE IMPLEMENTATION of this class - 9/9/16
  */
 public class BoatAnnotationView extends AnnotationView {
 
@@ -15,6 +22,7 @@ public class BoatAnnotationView extends AnnotationView {
 
     public BoatAnnotationView(Context context) {
         super(context);
+        this.init();
     }
 
     public BoatAnnotationView(Context context, AttributeSet attrs) {
@@ -27,6 +35,14 @@ public class BoatAnnotationView extends AnnotationView {
 
     public BoatAnnotationView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+
+    }
+
+    private void init() {
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageResource(R.drawable.smallboat);
+
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(100, 100)
     }
 
     @Override
